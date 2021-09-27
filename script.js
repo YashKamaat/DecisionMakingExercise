477c26653ae55c8165dad8d20b255954cbdaad16
Yash Kamaat <yashtkamaat@gmail.com>
2021-09-27 11:46:42 -0400

    edit

--- script.js
+++ script.js
@@ -87,6 +87,28 @@ switch (randomNum) {
     default:
         console.log("Five");
 }
+
+// or
+
+
+console.log("Random Number:", randomNum);
+switch (randomNum){
+  case 1:
+    console.log("One");
+    break;
+  case 2:
+    console.log("Two");
+    break;
+  case 3:
+    console.log("Three");
+    break;
+  case 4:
+    console.log("Four");
+    break;
+  case 5:
+    console.log("Five");
+    break;
+}
 // ================
 // HA! found the correct way to do this switch on MDN web docs
-// Missed the : for the : for my bonus 5
+// Missed the : for the : for my bonus five
