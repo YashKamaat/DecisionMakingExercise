aa5397dfb624e56657b6eafa6fe6209a38b8a987
Yash Kamaat <yashtkamaat@gmail.com>
2021-09-27 09:19:55 -0400

    Complete

--- script.js
+++ script.js
@@ -35,4 +35,21 @@ if (randomNum === (2 || 4)){
 randomNum >= 4 ? console.log("Equal to or more than 4") : console.log("Less than 4");
 // ================
 // 3
+switch (randomNum){
+    case 1:
+        console.log("One");
+        break;
+    case 2:
+        console.log("Two");
+        break;
+    case 3:
+        console.log("Three");
+        break;
+    case 4:
+        console.log("Four");
+        break;
+    case 5:
+        console.log("Five");
+        break;
+}
 // ================
