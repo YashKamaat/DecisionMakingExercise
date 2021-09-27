8ce1740ba5271851178e0c6e31f6b76095fa24ac
Yash Kamaat <yashtkamaat@gmail.com>
2021-09-27 11:11:38 -0400

    !==

--- script.js
+++ script.js
@@ -5,14 +5,35 @@ console.log(randomNum);
 // ================
 // 1b
 // I wanted to try if if instead of if else if, it let's both be executed instead of just one statement
+// if (randomNum >= 4) {
+//     console.log("Equal to or more than 4");
+// }
+// if (randomNum >= 2) {
+//     console.log("Equal to or more than 2");
+// } else {
+//     console.log("Equal to 1");
+// }
+
+// if (randomNum >= 4) {
+//     // Same as line above
+//   if (randomNum > 3) {
+//     console.log("Greater than or equal to 4");
+//   } else if (randomNum >= 2) {
+//   // Same as line above
+//   } else if (randomNum > 1) {
+//     console.log("Equal to 2 or 3");
+//   } else {
+//     console.log("Equal to 1");
+//   }
+
 if (randomNum >= 4) {
-    console.log("Equal to or more than 4");
-}
-if (randomNum >= 2) {
-    console.log("Equal to or more than 2");
-} else {
+    console.log("Greater than or equal to 4");
+  } else if (randomNum == 2 || randomNum == 3) {
+    console.log("Equal to 2 or 3");
+  } else {
     console.log("Equal to 1");
-}
+  }
+  
 // ================
 // 1c
 if (randomNum != 3) {
@@ -20,10 +41,14 @@ if (randomNum != 3) {
 }
 // ================
 // 1d
-if (randomNum != 3 && randomNum != 5) {
-    // if (randomNum != (3&&5)){
+ if (randomNum !== (3&&5)){
     console.log("Not equal to 3 AND Not equal to 5");
-}
+ }
+
+// OR
+console.log(randomNum);
+if (randomNum !== 3 && randomNum !== 5){
+  console.log("NOT equal to 3 AND not equal to 5");
 // ================
 // 1e
 if (randomNum === (2 || 4)) {
